a4d2c649540063901124adf86e639f9fb80efab5
Ram86889 <dogiparthiram@gmail.com>
2024-05-27 16:34:32 +0530

    changed login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
-import "./Login.css";
+
 const Login = () => {
     return(
-        <div className="secondMain">i am alog in component</div>
+        <div className="secondMain">i am a log in page</div>
     )
 };
 export default Login;
